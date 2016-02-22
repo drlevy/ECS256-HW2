e7245f3bb3c6fbc972f790270ba460f54c1a60ae
drlevy <daniel.levy@gmail.com>
2016-02-22 14:11:31 -0800

    Super simple test case for manual g.

--- Project/R/mmfitfunc.R
+++ Project/R/mmfitfunc.R
@@ -5,3 +5,23 @@ mmfitfunc <- function(x, g, start, lower = NULL, upper = NULL) {
 
   return(mmf)
 }
+
+#' @title testmmfitfunc
+#' @description testmmfitfunc
+#' @export
+testmmfitfunc <- function() {
+  x <- rexp(1000, 0.5)
+  mmf <- mmfit(x, gexp, 0.1, 0, 1)
+  hist(x, probability = TRUE)
+  minx <- min(x)
+  curve(dexp(x, mmf$thetahat), add = TRUE)
+  return(mmf)
+}
+
+gexp <- function(th, x) {
+  t1 <- th[1]
+  meanb <- 1/t1
+  m1 <- meanb-x
+  f <- cbind(m1)
+  return(f)
+}
