628d436b190046bfe3787c7241fa2b3fc6e3ecce
luan0731 <louisnguyen0731@gmail.com>
2016-02-22 10:57:19 -0800

    added

--- Project/R/mmfitgamma.R
+++ Project/R/mmfitgamma.R
@@ -1,5 +1,20 @@
 mmfitgamma <- function(x, start) {
-  mmf <- mmf()
+  samplesize = length(x);
 
+  g<-function(th,x){
+    alph<-th[1];
+    beta<-th[2];
+    mean<-alph/beta;
+    m1<-mean-x;
+    m2<- (alph/(beta^2)) - (x - mean)^2;
+    f <- cbind(m1, m2)
+    return (f);
+  }
+
+  ll = min(x); up = max(x)
+  coefs = gmmhelper(x, g, start, lower= ll, upper=up) # maybe?
+  hats = sqrt( coefs/length(samplesize) );
+
+  mmf <- mmf(thetahat = coefs, thetahatses = hats, denscomp = NULL, cdfband = NULL);
   return(mmf)
 }
--- Project/R/mmfitpoisson.R
+++ Project/R/mmfitpoisson.R
@@ -1,5 +1,17 @@
 mmfitpoisson <- function(x, start) {
-  mmf <- mmf()
+  samplesize = length(x);
 
+  g<-function(th,x){
+    lambda<-th[1];
+    mean<-lambda;
+    # lambda is mean
+    m1<-mean-x;
+    return(m1);
+  }
+
+  coefs = gmmhelper(x, g, start, lower=0.1, upper=999) # hmm...
+  hats = sqrt( res/length(samplesize) );
+
+  mmf <- mmf(thetahat = coefs, thetahatses = hats, denscomp = NULL, cdfband = NULL);
   return(mmf)
 }
