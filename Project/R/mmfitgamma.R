766b509b5b8b28fed7f48f84e5012da094a84968
drlevy <daniel.levy@gmail.com>
2016-02-22 14:29:36 -0800

    Re-deleting test harness and moving new test case.

--- Project/R/mmfitgamma.R
+++ Project/R/mmfitgamma.R
@@ -21,3 +21,15 @@ mmfitgamma <- function(x, start) {
 
 
 gammafit <- function(x, a, s) (x^(a-1))*exp(-x/s)/((s^a)*(gamma(a)))
+
+#' @title testgamma
+#' @description testgamma
+#' @export
+testgamma <- function(){
+  xb <- rgamma(1000, 5, 1/0.5)
+  mmf <- mmfit(xb, "gamma", c(2.1, 1/0.1))
+  hist(xb, probability = TRUE)
+  a<- mmf$thetahat[1]
+  s<- mmf$thetahat[2]
+  curve(dgamma(x, a, s), xlim = c(0,10), add = TRUE)
+}
