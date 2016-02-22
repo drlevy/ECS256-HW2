61cdc59899a1130a7e951d572b778882a9c9297b
drlevy <daniel.levy@gmail.com>
2016-02-21 22:21:53 -0800

    Initial gmm helper.

--- Project/R/mmfitshared.R
+++ Project/R/mmfitshared.R
@@ -1,3 +1,7 @@
-gmmhelper <- function() {
-
+gmmhelper <- function(g, x, start) {
+  if(NCOL(start) > 1) {
+    gmm(g, x, start)$coefficients
+  } else {
+    gmm(g, x, start, lower = 0, upper = 10, method = "Brent")$coefficients
+  }
 }
