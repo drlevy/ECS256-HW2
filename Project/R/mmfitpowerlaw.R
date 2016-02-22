4340309f1264a50b874985a4a4f8656a86ca1683
drlevy <daniel.levy@gmail.com>
2016-02-22 14:06:24 -0800

    Cleaning up power law test. Removing test harness.

--- Project/R/mmfitpowerlaw.R
+++ Project/R/mmfitpowerlaw.R
@@ -19,6 +19,18 @@ gpowerlaw <- function(th, x) {
   return(f)
 }
 
+#' @title testpowerlaw
+#' @description testpowerlaw
+#' @export
+testpowerlaw <- function() {
+  x <- rpowerlaw(1000, 2.7)
+  mmf <- mmfit(x, "power_law", 2.1)
+  hist(x, probability = TRUE)
+  minx <- min(x)
+  curve(dpowerlaw2(x, mmf$thetahat, minx), add = TRUE)
+  return(mmf)
+}
+
 mpowerlaw <- function(x, k) min(x)*(k-1)/(k-2)
 dpowerlaw2 <- function(x, k, xmin) ((k-1)/xmin)*(x/xmin)^(-k)
 dpowerlaw <- function(x, k) ((k-1)/min(x))*(x/(min(x)))^(-k)
