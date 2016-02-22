724ce5431eb8ddb8ccbf8fec8c8ecdad56135b27
drlevy <daniel.levy@gmail.com>
2016-02-21 22:24:07 -0800

    Fixed gmm helper.

--- Project/R/mmfitshared.R
+++ Project/R/mmfitshared.R
@@ -1,5 +1,5 @@
 gmmhelper <- function(g, x, start) {
-  if(NCOL(start) > 1) {
+  if(NROW(start) > 1) {
     gmm(g, x, start)$coefficients
   } else {
     gmm(g, x, start, lower = 0, upper = 10, method = "Brent")$coefficients
