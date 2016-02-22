6d9ad1cacff8c4c8592b8db7628b840580129e96
drlevy <daniel.levy@gmail.com>
2016-02-21 22:34:02 -0800

    Lazy boundary scaling for single coeff.

--- Project/R/mmfitshared.R
+++ Project/R/mmfitshared.R
@@ -2,6 +2,7 @@ gmmhelper <- function(g, x, start) {
   if(NROW(start) > 1) {
     gmm(g, x, start)$coefficients
   } else {
-    gmm(g, x, start, lower = 0, upper = 10, method = "Brent")$coefficients
+    # TODO: add better scaling of boundaries.
+    gmm(g, x, start, lower = 0, upper = start*2, method = "Brent")$coefficients
   }
 }
