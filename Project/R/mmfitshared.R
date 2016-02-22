34e690de64eede6b8e18d4f7dbed57dca755ff09
drlevy <daniel.levy@gmail.com>
2016-02-21 23:16:20 -0800

    Playing with scaling.

--- Project/R/mmfitshared.R
+++ Project/R/mmfitshared.R
@@ -2,7 +2,6 @@ gmmhelper <- function(g, x, start) {
   if(NROW(start) > 1) {
     gmm(g, x, start)$coefficients
   } else {
-    # TODO: add better scaling of boundaries.
-    gmm(g, x, start, lower = 0, upper = start*2, method = "Brent")$coefficients
+    gmm(g, x, start, lower = min(0, min(x)), upper = max(start*2, max(x)), method = "Brent")$coefficients
   }
 }
